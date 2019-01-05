cd11bd418799c0b573457e096c405bd2dcb86021
Robert Sherling <risaf1231@gmail.com>
2019-01-05 11:20:44 +0900

    Initialise repo

---
+++ test/test_helper.rb
@@ -0,0 +1,16 @@
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+
+# Refer to below link on how to use the minitest-reporters gem
+# https://github.com/kern/minitest-reporters
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
