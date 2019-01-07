f781f4da5757390d8c21f29cbd4183241372a28f
Risa Fujii <risaf1231@gmail.com>
2019-01-07 09:38:41 +0900

    Add Twilio gem, test message can be sent

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,9 @@
+# Copied from here, except change "secrets" to "credentials" due to Rails version change:
+# https://www.twilio.com/blog/2017/12/send-sms-ruby-rails-5-coffee.html
+
+Twilio.configure do |config|
+  config.account_sid = Rails.application.credentials.twilio_account_sid
+  config.auth_token = Rails.application.credentials.twilio_auth_token
+end
+
+TwilioClient = Twilio::REST::Client.new
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
-class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+# Don't actually run this test
+test 'save code' do
+  client = Twilio::REST::Client.new
+  client.messages.create(
+    from: Rails.application.credentials.twilio_phone_number,
+    to: '+818053800431',
+    body: 'Hello there! This is a test'
+  )
 end
