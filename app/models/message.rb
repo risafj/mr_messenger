62537bb29fc3994bb3934b7a89811f358dd2c3b1
Risa Fujii <risaf1231@gmail.com>
2019-01-08 17:37:59 +0900

    Add callback for receiver number as well

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,12 +4,17 @@ class Message < ApplicationRecord
 
   # Adding callback to verify that sender/receiver numbers are actually numbers beginning with +.
   # ASK - Method doesn't work yet, as data can be saved to db even if the sender number doesn't match regex, like "abc".
-  before_save :check_sender_num_validity
+  before_save :check_sender_num_validity, :check_receiver_num_validity
 
   private
 
   def check_sender_num_validity
     data_check = /^\+\d+$/.match(sender_number)
+    throw :abort if data_check.blank?
+  end
+
+  def check_receiver_num_validity
+    data_check = /^\+\d+$/.match(receiver_number)
     throw :abort if data_check.blank?
   end
 end
