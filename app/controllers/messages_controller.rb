cdb721b3761abeedd2ebd73664cfac1393b536ac
Risa Fujii <risaf1231@gmail.com>
2019-01-08 15:46:55 +0900

    Update create method to raise 400 error if info is incomplete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,23 +2,32 @@ class MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    new_message = Message.create(
+    new_message = Message.new(
       scheduled_date_time: message_params[:scheduled_date_time],
       sender_number: message_params[:sender_number],
       receiver_number: [:receiver_number],
       content: message_params[:content]
     )
 
-    render json: create_return_json(new_message, 'Message successfully scheduled')
+    if new_message.save
+      render json: { message: 'Message successfully scheduled' }
+    else
+      render json: new_message.errors.messages, status: :bad_request
+    end
   end
 
-  def delete; end
+  def delete
+    # message_to_delete = ...
+  end
 
   private
 
-  def create_return_json(error_check_object, success_message)
-    error_check_object.errors.messages.presence ? error_check_object.errors.messages : { message: success_message }
-  end
+  # def create_return_json(error_check_object, success_message)
+  #   if error_check_object.errors.messages.presence
+  #     error_check_object.errors.messages, status: :bad_request
+  #   else { message: success_message }
+  #   end
+  # end
 
   def message_params
     params.require(:message).permit(:scheduled_date_time, :sender_number, :receiver_number, :content)
