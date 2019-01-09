4b9ba9f9f76502e8dd352902ac5b8e1bf1f720f3
Risa Fujii <risaf1231@gmail.com>
2019-01-09 18:40:57 +0900

    Add controller test (delete request status 400)

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class MessagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     Rails.application.load_seed
-    @message_params = {
+  end
+
+  test 'should create message' do
+    post '/messages/', params: {
       message: {
         scheduled_date_time: '2019-01-10 22:00:00',
         sender_number: '+123456789',
@@ -11,10 +14,6 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
         content: 'controller test'
       }
     }
-  end
-
-  test 'should create message' do
-    post '/messages/', params: @message_params
     assert_response :success
     assert response.body.include? 'id'
   end
@@ -26,5 +25,8 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'delete request should return status 400 if id does not exist' do
+    assert Message.where(id: '99999').blank?
+    delete '/messages/99999/'
+    assert_response :bad_request
   end
 end
